9ff54c73884b9f777798a85528bbf44b8292c51d
Gyoza-Gyoza <136580712+Gyoza-Gyoza@users.noreply.github.com>
2025-10-24 15:11:06 +0800

    fixed knockback during death

--- Assets/Entities/Entity.cs
+++ Assets/Entities/Entity.cs
@@ -60,7 +60,7 @@ public abstract class Entity : MonoBehaviour
     public virtual void TakeDamage(int amount)
     {
         health -= amount;
-        if (health > 0) OnDamage();
+        OnDamage();
         if (health <= 0)
         {
             health = 0; // Ensure health doesn't go below zero
